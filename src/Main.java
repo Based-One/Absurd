b18a29de4085fc964cb450442d648faa58cd232b
Diego Esteban <140222536+Based-One@users.noreply.github.com>
2023-09-17 10:07:15 -0700

    Just an example

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-
+        System.out.println("Bye and thank you");
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
